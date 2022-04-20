53875bd2eb5f2d825945191f2225a809eac30fe6
paezn <npaez@colegioguanenta.edu.co>
2022-04-20 07:43:56 -0500

    Desarrollo: programa terminado en python. paezn

---
+++ README.md
@@ -0,0 +1,5 @@
+# Programa No. 2
+
+### Programa para calcular x a la y.
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ potencia.py
@@ -0,0 +1,17 @@
+"""Programa No. 2:
+Programa para calcular x a la y"""
+
+print("--------------------------------")
+print("----Potencia x a la y ----------")
+print("--------------------------------")
+
+# input
+x = int(input("Digite el valor de x: "))
+y = int(input("Digite el valor de y: "))
+
+# processing
+z = x**y
+
+# output
+print("\nResultados:\n")
+print(str(x) + " a la " + str(y) + " es igual a " + str(z))
